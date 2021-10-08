20af90bbea397f028489b8a81a9b7bf68c1a887f
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-08 18:50:02 +0100

    Add BaseAccountBalanceException class

--- src/main/java/com/mrdiipo/digicore_banking_app/controller/DigicoreController.java
+++ src/main/java/com/mrdiipo/digicore_banking_app/controller/DigicoreController.java
@@ -5,7 +5,9 @@ import com.mrdiipo.digicore_banking_app.controller.request.DepositRequest;
 import com.mrdiipo.digicore_banking_app.controller.request.LoginRequest;
 import com.mrdiipo.digicore_banking_app.controller.request.WithdrawalRequest;
 import com.mrdiipo.digicore_banking_app.controller.response.*;
+import com.mrdiipo.digicore_banking_app.exception.AccountDepositException;
 import com.mrdiipo.digicore_banking_app.exception.AccountNotFoundException;
+import com.mrdiipo.digicore_banking_app.exception.BaseAccountBalanceException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,7 +36,7 @@ public class DigicoreController {
     @PostMapping(path = "/deposit",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public DepositResponse deposit(@RequestBody DepositRequest depositRequest){
+    public DepositResponse deposit(@RequestBody DepositRequest depositRequest) throws AccountDepositException, AccountNotFoundException {
         return null;
     }
 
@@ -42,7 +44,7 @@ public class DigicoreController {
     @PostMapping(path = "/withdrawal",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public WithdrawalResponse withdrawal(@RequestBody WithdrawalRequest withdrawalRequest){
+    public WithdrawalResponse withdrawal(@RequestBody WithdrawalRequest withdrawalRequest) throws AccountNotFoundException, BaseAccountBalanceException {
 
         return null;
     }
