281bd554ea914de0bfa1a6f4364c8bcbae2d372c
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-08 09:36:09 +0100

    Initial commit

---
+++ src/main/java/com/mrdiipo/digicore_banking_app/DigicoreBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.digicore_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicoreBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigicoreBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrdiipo/digicore_banking_app/DigicoreBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.digicore_banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigicoreBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
