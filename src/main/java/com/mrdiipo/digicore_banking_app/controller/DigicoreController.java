4b5011c9100528ce576b3b548ae375b9f55b03a2
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-08 13:41:07 +0100

    Add controller endpoints

---
+++ src/main/java/com/mrdiipo/digicore_banking_app/controller/DigicoreController.java
@@ -0,0 +1,23 @@
+package com.mrdiipo.digicore_banking_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DigicoreController {
+
+    @GetMapping("/account_info/{accountNumber}")
+
+    @GetMapping("/account_statement/{accountNumber}")
+
+    @PostMapping("/deposit")
+
+    @PostMapping("/withdrawal")
+
+    @PostMapping("/create_account")
+
+    @PostMapping("/login")
+
+
+}
