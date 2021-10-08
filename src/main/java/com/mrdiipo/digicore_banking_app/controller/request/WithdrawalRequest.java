6f356151545fb96a577ef9dcf5d0b6a22f99dc37
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-08 14:59:35 +0100

    Add WithdrawalRequest class

---
+++ src/main/java/com/mrdiipo/digicore_banking_app/controller/request/WithdrawalRequest.java
@@ -0,0 +1,17 @@
+package com.mrdiipo.digicore_banking_app.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+// Rest request object class for withdrawal transactions
+public final class WithdrawalRequest {
+
+    private final String accountNumber;
+    private final String accountPassword;
+    private final BigDecimal withdrawnAmount;
+
+}
