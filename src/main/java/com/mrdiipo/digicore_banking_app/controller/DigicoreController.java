ad68169895920345d276847120073cba82157411
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-08 15:43:24 +0100

    Update controller

--- src/main/java/com/mrdiipo/digicore_banking_app/controller/DigicoreController.java
+++ src/main/java/com/mrdiipo/digicore_banking_app/controller/DigicoreController.java
@@ -1,23 +1,50 @@
 package com.mrdiipo.digicore_banking_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mrdiipo.digicore_banking_app.controller.request.CreateAccountRequest;
+import com.mrdiipo.digicore_banking_app.controller.request.DepositRequest;
+import com.mrdiipo.digicore_banking_app.controller.request.LoginRequest;
+import com.mrdiipo.digicore_banking_app.controller.request.WithdrawalRequest;
+import com.mrdiipo.digicore_banking_app.controller.response.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+// Class containing endpoints for the application
 public class DigicoreController {
 
+    /*Get Mappings*/
+
     @GetMapping("/account_info/{accountNumber}")
+    public AccountInfoResponse getAccountInfo(@PathVariable String accountNumber){
+        return null;
+    }
 
     @GetMapping("/account_statement/{accountNumber}")
+    public AccountStatementResponse getAccountStatement(@PathVariable String accountNumber){
+        return null;
+    }
+
+    /*Post Mappings*/
 
     @PostMapping("/deposit")
+    public DepositResponse deposit(@RequestBody DepositRequest depositRequest){
+        return null;
+    }
+
 
     @PostMapping("/withdrawal")
+    public WithdrawalResponse withdrawal(@RequestBody WithdrawalRequest withdrawalRequest){
+        return null;
+    }
 
     @PostMapping("/create_account")
+    public CreateAccountResponse createAccount(@RequestBody CreateAccountRequest createAccountRequest){
+        return null;
+    }
 
     @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest){
+        return null;
+    }
 
 
 }
